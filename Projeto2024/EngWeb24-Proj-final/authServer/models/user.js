cb2c76dbfd2553760a2ce7d1ae5c6445070ee77e
andrepfilipe01 <andre.filipe_01@hotmail.com>
2024-06-24 11:32:47 +0100

    versão final. A outra tinha um pequeno bug

---
+++ Projeto2024/EngWeb24-Proj-final/authServer/models/user.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose')
+var passportLocalMongoose = require('passport-local-mongoose')
+
+var userSchema = new mongoose.Schema({
+    name: String,
+    username: String, 
+    password: String,
+    level: String, 
+    dateCreated: Date, 
+    lastAccess: Date, 
+},{versionKey: false})
+
+userSchema.plugin(passportLocalMongoose)
+
+module.exports = mongoose.model('user', userSchema)
